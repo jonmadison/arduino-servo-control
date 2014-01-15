d1c33772f9b6d33cec84eb10ac70c20821e6db96
jon madison <jon@tfftech.com>
2014-01-14 16:27:22 -0800

    don't use continuous API

--- servo.js
+++ servo.js
@@ -10,16 +10,15 @@ board.on("ready", function() {
   console.log("board ready. left right arrow keys, 'q' to quit.");
   var servo = new five.Servo({
     "pin": 9,
-    "startAt":program.startAt,
-    "type":"continuous"
+    "startAt":program.startAt
     });
   process.stdin.on('keypress', function (ch, key) {
     // console.log('got "keypress"', key);
     if (key && key.name == 'right') {
-      servo.cw(1);
+      servo.to(0);
     }
     if (key && key.name == 'left') {
-      servo.ccw(1);
+      servo.to(180);
     }
     if(key && key.name == 'q') {
       process.exit(0);
